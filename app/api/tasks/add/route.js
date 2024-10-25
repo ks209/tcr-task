96eb2f07e232d5221e45f5a4284a91e4882cba7f
ks2092003 <ks2092003@gmail.com>
2024-10-25 23:23:41 +0530

    fixed db3

--- app/api/tasks/add/route.js
+++ app/api/tasks/add/route.js
@@ -1,4 +1,4 @@
-import db from "@/app/utils/db";
+import db from "..//utils/db";
 import { NextResponse } from "next/server";
 
 
--- app/utils/db.js
+++ app/utils/db.js
@@ -2,7 +2,45 @@ import { JSONFilePreset } from 'lowdb/node'
 
 
 
-const db = await JSONFilePreset('db.json', { tasks: [], teams: ["Dhruv", "Harshit", "Manav", "Ashtmi", "Kartik"], clients: ["Cazzano", "NoMushkil", "Aone", "TrexPro"] })
+const db = await JSONFilePreset('/tmp/db.json', {
+  "tasks": [
+    {
+      "id": "1728893479789",
+      "description": "kkkkkkkkkkk",
+      "member": "Dhruv",
+      "client": "Cazzano",
+      "status": "in-progress"
+    },
+    {
+      "id": "1728893481763",
+      "description": "kkkkkkkkkkk",
+      "member": "Dhruv",
+      "client": "Cazzano",
+      "status": "completed"
+    },
+    {
+      "id": "1729848830992",
+      "description": "This is to inform the members of tricore revenue that Task App for Tricore is Successful ",
+      "member": "Kartik",
+      "client": "Aone",
+      "status": "completed"
+    }
+  ],
+  "teams": [
+    "Manav",
+    "Ashtmi",
+    "Kartik",
+    "Praveen",
+    "Dhruv"
+  ],
+  "clients": [
+    "Aone",
+    "TrexPro",
+    "Cazzano",
+    "NoMushkil",
+    "Gillori"
+  ]
+})
 
 async function initDB() {
   await db.read();
